a6913c669cec4b889e49b7e224b8a5fdde7847da
Flowtaris Dev <dev@flowtaris.com>
2026-06-24 20:05:58 +0530

    chore: update robots.txt rules

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,62 +2,11 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: 'Googlebot',
-        allow: '/',
-        disallow: ['/admin/', '/api/', '/_next/', '/studio/'],
-      },
-      {
-        userAgent: 'Bingbot',
-        allow: '/',
-        disallow: ['/admin/', '/api/', '/_next/static/', '/studio/'],
-      },
-      {
-        userAgent: ['Googlebot-Image', 'Googlebot-Video', 'Slurp'],
-        allow: '/',
-      },
-      {
-        userAgent: [
-          'GPTBot',
-          'ChatGPT-User',
-          'anthropic-ai',
-          'ClaudeBot',
-          'PerplexityBot',
-          'YouBot',
-          'Google-Extended',
-          'CCBot',
-          'Applebot-Extended',
-          'cohere-ai',
-          'AI2Bot',
-          'Diffbot',
-          'Omgilibot',
-          'FacebookBot',
-        ],
-        allow: '/',
-        disallow: ['/admin/', '/api/', '/_next/static/', '/studio/'],
-      },
-      {
-        userAgent: 'Bytespider',
-        disallow: '/',
-      },
-      {
-        userAgent: ['AhrefsBot', 'SemrushBot'],
-        allow: '/',
-        disallow: ['/admin/', '/api/', '/_next/static/', '/studio/'],
-        crawlDelay: 1,
-      },
-      {
-        userAgent: ['MJ12bot', 'DotBot'],
-        allow: '/',
-        disallow: ['/admin/', '/api/', '/_next/static/', '/studio/'],
-      },
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/admin/', '/api/', '/_next/static/', '/studio/'],
-      },
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/api/', '/studio/'],
+    },
     sitemap: [
       'https://www.flowtaris.com/sitemap.xml',
       'https://www.flowtaris.com/sitemap-static.xml',
